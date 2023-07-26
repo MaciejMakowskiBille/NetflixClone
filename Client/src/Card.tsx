c62d1d0545461ce98c4796f7b88cc6cb07807db1
StachuDev <solszakdev@gmail.com>
2023-07-26 16:29:31 +0200

    initial changes

---
+++ Client/src/Card.tsx
@@ -0,0 +1,15 @@
+export default function Card() {
+  return (
+    <div className="wraper">
+      <p className="wraper__text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore optio
+        eaque repudiandae consequuntur natus enim tenetur cupiditate est et
+        adipisci rem doloribus vitae atque, modi eum dolores ipsam expedita
+        possimus sit vel voluptas, accusantium dolor hic delectus? Suscipit
+        repellat voluptas harum et error eum, atque omnis similique consequatur
+        officiis consequuntur.
+      </p>
+      <button>Kontynułuj</button>
+    </div>
+  );
+}
