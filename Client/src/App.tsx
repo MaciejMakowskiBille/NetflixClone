637d98d64d2f00cb1c786eb8e64c4862cd3456a4
StachuDev <solszakdev@gmail.com>
2023-07-31 22:09:32 +0200

    style fix

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,12 @@
 // import RegistrationEmail from "./pages/RegistrationEmail";
 // import RegistrationPassword from "./pages/RegistrationPassword";
-import RegistrationAgreements from "./pages/RegistrationAgreement";
+// import RegistrationAgreements from "./pages/RegistrationAgreement";
+import RegistrationPayments from "./pages/RegistrationPayments";
 
 function App() {
   return (
     <>
-      <RegistrationAgreements />
+      <RegistrationPayments />
     </>
   );
 }
