95e0d0b92ee5123ce12ad302a293607da559e834
StachuDev <solszakdev@gmail.com>
2023-08-02 16:55:00 +0200

    tidy up styles

--- Client/src/components/TextBox.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function TextBox({
-  child,
-}: {
-  child?: ReactNode[] | ReactNode;
-}) {
-  return <div className="text-box">{child}</div>;
-}
