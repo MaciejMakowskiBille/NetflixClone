8010123a541fa4b7a1c15ab24c9826c925e528e5
MaciejMakowskiBille <maciej.makowski@billennium.com>
2023-08-03 10:02:12 +0200

    Fix links in MainPage

--- Client/src/pages/mainPage/mainPage.tsx
+++ Client/src/pages/mainPage/mainPage.tsx
@@ -1,14 +1,17 @@
-import Navigation from "../../components/Navigation/nav"
-import disney from '../../imgs/producers/disney.png'
-import pixar from '../../imgs/producers/pix.png'
-import marvel from '../../imgs/producers/marv.png'
-import geo from '../../imgs/producers/geo.png'
-import star from '../../imgs/producers/star.png'
-import dc from '../../imgs/producers/dc.png'
-import Producer from "./components/producer"
+import Navigation from "../../components/nav"
+import disney from '../imgs/producers/disney.png'
+import pixar from '../imgs/producers/pix.png'
+import marvel from '../imgs/producers/marv.png'
+import geo from '../imgs/producers/geo.png'
+import star from '../imgs/producers/star.png'
+import dc from '../imgs/producers/dc.png'
+import Producer from "../home/producer"
 import CategoryRow from "../../components/categoryRow"
+import small from '../imgs/testTile/mini.png'
+import hovered from '../imgs/testTile/hover.png'
+import logo from '../imgs/testTile/logo.png'
 import { getFilms } from "../../utils/Gets"
-import {useState, useEffect} from 'react'
+import { FilmListType } from "../../types/mainPage"
 
 const producerList = [
     {
@@ -37,16 +40,60 @@ const producerList = [
     }
 ]
 
+const list:FilmListType = [
+    {
+        title:"The Walking Dead",
+        categories:[{id:1, name:'Dramatyczny'}, {id:2,name:'Przygodowy'}, {id:3, name:"Amerykański"}],
+        img:small,
+        hoverImg:hovered,
+        logo:logo,
+        ageCategory:18,
+        premiere: new Date ("2021-12-22"),
+        transcription:true
+    },
+    {
+        title:"The Walking Dead",
+        categories:[{id:1, name:'Dramatyczny'}, {id:2,name:'Przygodowy'}, {id:3, name:"Amerykański"}],
+        img:small,
+        hoverImg:hovered,
+        logo:logo,
+        ageCategory:18,
+        premiere: new Date ("2021-12-22"),
+        transcription:true
+    },
+    {
+        title:"The Walking Dead",
+        categories:[{id:1, name:'Dramatyczny'}, {id:2,name:'Przygodowy'}, {id:3, name:"Amerykański"}],
+        img:small,
+        hoverImg:hovered,
+        logo:logo,
+        ageCategory:18,
+        premiere: new Date ("2021-12-22"),
+        transcription:true
+    },
+    {
+        title:"The Walking Dead",
+        categories:[{id:1, name:'Dramatyczny'}, {id:2,name:'Przygodowy'}, {id:3, name:"Amerykański"}],
+        img:small,
+        hoverImg:hovered,
+        logo:logo,
+        ageCategory:18,
+        premiere: new Date ("2021-12-22"),
+        transcription:true
+    },
+    {
+        title:"The Walking Dead",
+        categories:[{id:1, name:'Dramatyczny'}, {id:2,name:'Przygodowy'}, {id:3, name:"Amerykański"}],
+        img:small,
+        hoverImg:hovered,
+        logo:logo,
+        ageCategory:18,
+        premiere: new Date ("2021-12-22"),
+        transcription:true
+    },
+]
+
 const MainPage = () => {
-    const [moviesData, setMoviesData] = useState<MovieDataType[] | null>(null)
-    const [isLoading, setIsLoading] = useState(true)
-    useEffect(() => {
-        getFilms().then((res) => {
-            setMoviesData(prev => prev = res)
-            console.log(res)
-            setIsLoading(prev => prev = false)
-        })
-    },[])
 
     getFilms()
     return(
@@ -65,9 +112,7 @@ const MainPage = () => {
                         )
                     })}
                 </div>
-                {!isLoading && moviesData && (
-                    <CategoryRow title="Ostatnio popularne" moviesList={moviesData}/>
-                )}
+                <CategoryRow title="Ostatnio popularne" moviesList={list}/>
 
             </main>
         </div>
