fa6fff7e877a64ae63ae83f020ca7fa395eadc55
MaciejMakowskiBille <maciej.makowski@billennium.com>
2023-08-07 12:02:44 +0200

    Separate functions in helpers

--- Client/src/utils/helpers.ts
+++ Client/src/utils/helpers.ts
@@ -1,25 +1,6 @@
 
 export const clearMovieData = (data:MovieResponseType) => {
-    const clearedCast:Actor[] = data.attributes.cast.data.map((actor) => {
-        return (
-          {
-            id:actor.id,
-            firstName:actor.attributes.firstName,
-            lastName:actor.attributes.lastName,
-            birthDate:actor.attributes.birthDate,
-            image:actor.attributes.image.data.attributes.url
-          }
-        )
-      })
-      const clearedCategory:Category[] = clearCategoryData(data.attributes.categories.data)
 
-      const clearedDirector:Director = {
-        id: data.attributes.director.data.id,
-        firstName: data.attributes.director.data.attributes.firstName,
-        lastName: data.attributes.director.data.attributes.lastName,
-        birthDate: data.attributes.director.data.attributes.birthDate,
-        image: data.attributes.director.data.attributes.image.data.attributes.url
-      }
       const clearedData:MovieDataType = {
         id:data.id,
         title:data.attributes.title,
@@ -32,9 +13,9 @@ export const clearMovieData = (data:MovieResponseType) => {
         hoverImg:data.attributes.hoverImg.data.attributes.url,
         logo:data.attributes.logo.data.attributes.url,
         duration:data.attributes.duration,
-        categories:clearedCategory,
-        cast:clearedCast,
-        director:clearedDirector,
+        categories:clearCategoryData(data.attributes.categories.data),
+        cast:clearCastData(data.attributes.cast.data),
+        director:clearDirectorData(data.attributes.director.data),
         ageCategory:data.attributes.ageCategory,
         transcription:data.attributes.transcription
       }
@@ -52,4 +33,31 @@ export const clearCategoryData = (data:CategoryResponseType[]) => {
   })
   
   return clearedCategories
+}
+
+export const clearDirectorData = (data:DirectorResponseType) => {
+  const clearedDirector:Director = {
+    id: data.id,
+    firstName: data.attributes.firstName,
+    lastName: data.attributes.lastName,
+    birthDate: data.attributes.birthDate,
+    image: data.attributes.image.data.attributes.url
+  }
+  return clearedDirector
+}
+
+export const clearCastData = (data:ActorResponseType[]) => {
+  const clearedCast:Actor[] = data.map((actor) => {
+    return (
+      {
+        id:actor.id,
+        firstName:actor.attributes.firstName,
+        lastName:actor.attributes.lastName,
+        birthDate:actor.attributes.birthDate,
+        image:actor.attributes.image.data.attributes.url
+      }
+    )
+  })
+
+  return clearedCast
 }
