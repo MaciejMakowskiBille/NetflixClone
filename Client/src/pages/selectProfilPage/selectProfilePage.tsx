d815115719ad6cd7c6da37746e68b4ef3c61f3f2
MaciejMakowskiBille <maciej.makowski@billennium.com>
2023-08-22 13:30:41 +0200

    Create AddProfileModal with styles

--- Client/src/pages/selectProfilPage/selectProfilePage.tsx
+++ Client/src/pages/selectProfilPage/selectProfilePage.tsx
@@ -1,9 +1,16 @@
 import { useNavigate } from "react-router-dom"
 import ProfileTile from "./components/profileTile"
+import {useState} from 'react'
+import AddProfileModal from "./components/addPorfileModal"
 
 
 const SelectProfilePage = () =>{
     const navigate = useNavigate()
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    
+    const handleClickAddButton = () => {
+        setIsModalOpen(true)
+    }
 
     return(
         <div className="appBackground">
@@ -13,9 +20,12 @@ const SelectProfilePage = () =>{
                     <ProfileTile/>
                     <ProfileTile/>
                     <ProfileTile/>
-                    <div className="icon addButton iconButton"/>
+                    <div className="icon addButton iconButton" onClick={handleClickAddButton}/>
                 </section>
                     <button className="button-exit profile-exit-button" onClick={() => navigate("/")}>WYLOGUJ</button>
+                    {isModalOpen && (
+                        <AddProfileModal setIsModalOpen={setIsModalOpen}/>
+                    )}
             </main>
         </div>
     )
