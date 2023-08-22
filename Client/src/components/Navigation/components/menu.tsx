28e41e336a9a27e5f94348149acae8a561d80882
MaciejMakowskiBille <maciej.makowski@billennium.com>
2023-08-22 14:35:30 +0200

    fix styles in ProfilePage and change menu color

--- Client/src/components/Navigation/components/menu.tsx
+++ Client/src/components/Navigation/components/menu.tsx
@@ -1,13 +1,15 @@
+import { useNavigate } from "react-router-dom"
 
 type MenuProps = {
     isOpen:boolean
 }
 
 const Menu = (props:MenuProps) => {
+    const navigate = useNavigate()
 
     return(
         <div className={props.isOpen ? "menu open" : "menu close"}>
-            <div className="menu-item">
+            <div className="menu-item" onClick={() => navigate("/profile")}>
                 Profile
             </div>
             <div className="menu-item">
