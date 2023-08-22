b75a7f7b84d2c4cec14564be47ddcde3fcb45f2c
MaciejMakowskiBille <maciej.makowski@billennium.com>
2023-08-22 12:18:47 +0200

    Change App routes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,16 +4,20 @@ import FilteredMovies from "./pages/filteredMovies/filteredMovies";
 import './style/style.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProducersPage from "./pages/producersPage/producersPage";
+import SelectProfilePage from "./pages/selectProfilPage/selectProfilePage";
+import Home from "./pages/home/home";
 
 const App = () => {
 
   return (
     <Router>
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<MainPage />}/>
-        <Route path="movie/:movieType/:movieId" element={<MoviePage />} />
-        <Route path="list/:type/:filter" element={<FilteredMovies/>}/>
-        <Route path="producers" element={<ProducersPage/>}/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/main" element={<MainPage />}/>
+        <Route path="/profile" element={<SelectProfilePage />} />
+        <Route path="/movie/:movieType/:movieId" element={<MoviePage />} />
+        <Route path="/list/:type/:filter" element={<FilteredMovies/>}/>
+        <Route path="/producers" element={<ProducersPage/>}/>
       </Routes>
     </Router>
   )
