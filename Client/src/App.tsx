ecf3ab9e165135acefd51468681b86494ee1d3b6
kowalowy1234 <kowalowy1234@gmail.com>
2023-09-04 09:56:15 +0200

    App.tsx routes cleanup

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,11 @@
-
 import SignIn from "./pages/signIn/SignIn";
 import SignUp from "./pages/signUp/SignUp";
 
-import MainPage from "./pages/mainPage/mainPage"
-import MoviePage from "./pages/moviePage/moviePage"
+import MainPage from "./pages/mainPage/mainPage";
+import MoviePage from "./pages/moviePage/moviePage";
 import FilteredMovies from "./pages/filteredMovies/filteredMovies";
-import './style/style.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./style/style.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProducersPage from "./pages/producersPage/producersPage";
 import SelectProfilePage from "./pages/selectProfilPage/selectProfilePage";
 import Home from "./pages/home/home";
@@ -17,12 +16,12 @@ const App = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/signIn" element={<SignIn />} />
-        <Route path="/" element={<MainPage />}/>
+        <Route path="/" element={<Home />} />
         <Route path="/movie/:movieType/:movieId" element={<MoviePage />} />
-        <Route path="/main" element={<MainPage />}/>
+        <Route path="/main" element={<MainPage />} />
         <Route path="/profile" element={<SelectProfilePage />} />
-        <Route path="/list/:type/:filter" element={<FilteredMovies/>}/>
-        <Route path="/producers" element={<ProducersPage/>}/>
+        <Route path="/list/:type/:filter" element={<FilteredMovies />} />
+        <Route path="/producers" element={<ProducersPage />} />
       </Routes>
     </Router>
   );
