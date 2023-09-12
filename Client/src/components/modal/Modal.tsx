4e28cc0c6c6ba70002f96f783ba538b011014ec7
StachuDev <solszakdev@gmail.com>
2023-09-12 18:07:58 +0200

    modal component correction

--- Client/src/components/modal/Modal.tsx
+++ Client/src/components/modal/Modal.tsx
@@ -5,29 +5,36 @@ export default function Modal({
   btnText,
   children,
   setModalIsOpen,
+  onSubmit,
 }: {
   title: string;
   btnText: string[];
   children: ReactNode;
-  setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>> | undefined;
+  onSubmit: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }) {
   function handleCloseModal() {
-    setModalIsOpen(false);
+    setModalIsOpen?.(false);
   }
+
   return (
     <>
       <div className="backdrop" />
       <div className="modal">
         <header className="modal__header">
           <h2>{title}</h2>
-          <button
-            className="closeButton icon right"
-            onClick={handleCloseModal}
-          />
+          {setModalIsOpen && (
+            <button
+              className="closeButton icon right"
+              onClick={handleCloseModal}
+            />
+          )}
         </header>
         <div className="modal__content">{children}</div>
         <footer className="modal__actions">
-          <button className="button-primary">{btnText[0]}</button>
+          <button className="button-primary" onClick={onSubmit}>
+            {btnText[0]}
+          </button>
           {btnText.length === 2 && (
             <button className="button-secondary">{btnText[1]}</button>
           )}
